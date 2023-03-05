d0f1448d04fe06f5939dc58922eae5e1d77f556f
kyakivanfrank <kyakivanfrank@gmail.com>
2023-03-05 15:51:32 +0300

    start big days by asha repo

---
+++ README.md
@@ -0,0 +1,2 @@
+this is a simple web site project with use of React in the front End-end, node and mongoDb in the back_end.
+it's named ndongo website
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/styles.css'
+import logo from "./resources/big-days-by-asha-logo.svg"
+
+const App = () => {
+  return (
+    <>
+    <img src={logo} alt='logo' />
+
+    <h1>website coming soon</h1></>
+  )
+}
+
+export default App
